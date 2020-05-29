ae5658a82371989e328368392349db9f66e08f28
javiperera <65867631+javiperera@users.noreply.github.com>
2020-05-29 13:23:57 +0200

    Add files via upload

---
+++ programa.js
@@ -0,0 +1,59 @@
+const boton = document.querySelector(".boton");
+
+boton.addEventListener("click", function(){
+document.getElementById("sidebar").classList.toggle("active");
+})
+
+const comercio = document.querySelector(".comercio");
+
+comercio.addEventListener("click", function(){
+document.getElementById("sidebar2").classList.toggle("active");
+})
+
+const empresas = document.querySelector(".empresas");
+
+empresas.addEventListener("click", function(){
+document.getElementById("sidebar3").classList.toggle("active");
+})
+
+const españa = document.querySelector(".españa");
+
+españa.addEventListener("click", function(){
+document.getElementById("sidebar4").classList.toggle("active");
+})
+const impuestos = document.querySelector(".impuestos");
+
+impuestos.addEventListener("click", function(){
+document.getElementById("sidebar5").classList.toggle("active");
+})
+
+const menester = document.querySelector(".menester");
+
+menester.addEventListener("click", function(){
+document.getElementById("sidebar6").classList.toggle("active");
+})
+
+const mercadoFinanciero = document.querySelector(".mercadoFinanciero");
+
+mercadoFinanciero.addEventListener("click", function(){
+document.getElementById("sidebar7").classList.toggle("active");
+})
+
+const poderes = document.querySelector(".poderes");
+
+poderes.addEventListener("click", function(){
+document.getElementById("sidebar8").classList.toggle("active");
+})
+
+const recursos = document.querySelector(".recursos");
+
+recursos.addEventListener("click", function(){
+document.getElementById("sidebar9").classList.toggle("active");
+})
+
+const sostenibilidad = document.querySelector(".sostenibilidad");
+
+sostenibilidad.addEventListener("click", function(){
+document.getElementById("sidebar10").classList.toggle("active");
+})
+
